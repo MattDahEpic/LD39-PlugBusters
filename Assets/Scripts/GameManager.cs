d45459c9cd6288563a7385f6dc18a726a2e2cc46
MattDahEpic <mattdahepic@gmail.com>
2017-07-28 21:00:46 -0600

    Movement tech

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public int maxCords = 8;
+
+    public GameObject[] tenants;
+
+    [HideInInspector] public int currentCords;
+    [HideInInspector] public int score;
+
+    void Start () {
+        currentCords = maxCords;
+        score = 0;
+	}
+	void Update () {
+		
+	}
+}
